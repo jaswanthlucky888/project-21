277e9741079a974342860b1c065031c984ce7cd0
jaswanthlucky888 <85064953+jaswanthlucky888@users.noreply.github.com>
2021-07-04 12:55:21 -0400

    Add files via upload

---
+++ MatterJSBoilerPlate-main/ground.js
@@ -0,0 +1,31 @@
+class ground 
+{
+  constructor(x, y, w, h) 
+  {
+    let options = {
+      isStatic:true
+    };
+    
+    
+    this.x=x;
+    this.y=y;
+    this.w = w;
+    this.h = h;
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    rectMode(CENTER);
+    stroke(255);
+    fill("yellow");
+    rect(0,0, this.w, this.h);
+    pop();
+  }
+  
+}
+
+
